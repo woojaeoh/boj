a934b8e2248235b4c1a37051025f9c3e5c0347fb
woojaeoh <166394273+woojaeoh@users.noreply.github.com>
2024-09-04 16:22:38 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 6.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,9 +1,7 @@
 def solution(t, p):
     result = 0
-    length=len(p)
-    
-    for i in range(len(t)-length+1):
-        if int(t[i:i+length]) <= int(p):
+    for i in range(len(t)-len(p)+1):
+        if int(p) >= int(t[i:i+len(p)]):
             result+=1
             
     return result
