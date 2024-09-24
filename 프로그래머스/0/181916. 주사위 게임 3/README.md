af5bfe805dfa4cefbaffe240e78e47d23b3f9b0f
woojaeoh <166394273+woojaeoh@users.noreply.github.com>
2024-09-24 11:44:10 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/README.md
+++ 프로그래머스/0/181916. 주사위 게임 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 3 - 181916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 19일 16:13:43
+2024년 09월 24일 11:44:13
 
 ### 문제 설명
 
--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,18 +1,50 @@
 def solution(a, b, c, d):
-    arr = [a, b, c, d]
-    cnt = [arr.count(item) for item in arr]
-
-    if max(cnt)==4: return a*1111
-
-    elif max(cnt)==3:
-        return (10 * (arr[cnt.index(3)]) + (arr[cnt.index(1)]))**2       
-
-    elif max(cnt)==2:    
-        if 1 in cnt:
-            return arr[cnt.index(1)] * arr[cnt.index(1, cnt.index(1)+1,4)]
-        else:
-            for item in arr:
-                if a!=item:
-                    return (a+item) * abs(a-item)
-
-    else: return min(arr)
+    #4개가 다 같은 경우
+    if (a==b==c==d):
+        return a*1111
+    
+    #3개가 같고 한개가 다른 경우
+    if (a==b==c!=d):
+        return (10*a+d)**2
+    if (a==b==d!=c):
+        return (10*a+c)**2
+    if (a==c==d!=b):
+        return (10*a+b)**2
+    if (b==c==d!=a):
+        return (10*b+a)**2
+    
+    #두개씩 같은 경우
+    if (a==b) and (c==d):
+        return (a+c)*abs(a-c)
+    if (a==c) and (b==d):
+        return (a+b)*abs(a-b)
+    if (a==d) and (b==c):
+        return (a+b)*abs(a-b)
+    
+    #두개가 같고 나머지 하나 하나 다른 경우
+    if a==b or a==c or a==d :
+        same=a
+        others=[x for x in [b,c,d] if x!=a]
+        return others[0]* others[1] 
+    
+    if b==c or b==d :
+        same=b
+        others=[x for x in [a,c,d] if x!=b]
+        return others[0]* others[1]
+    
+    if c==d:
+        same=c
+        others=[x for x in [a,b,d] if x!=c]
+        return others[0]*others[1]
+    
+    
+    
+    if a!= b!= c!= d:
+        return min(a,b,c,d)
+    
+    
+    
+    
+    
+    
+  
