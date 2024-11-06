0bf885f29aa5bf1955ece18fb63e5f21ce446d1c
woojaeoh <166394273+woojaeoh@users.noreply.github.com>
2024-11-06 13:55:05 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,12 +1,23 @@
 def solution(a, b):
-    #아이디어 1. 
-    answer = ''
+    #2016년 a월 b일은 어떤 날인지?
+    #2016년은 윤년
+    #2016년 a월 b일은 실제로 있는 날 ex) 13월 26일 , 2월 45일 X
+    
     day=["THU","FRI","SAT","SUN","MON","TUE","WED"]
-    month_days=[31,29,31,30,31,30,31,31,30,31,30,31]
-    total_days=0
-    for i in range(a-1):
-        total_days+=month_days[i]
-    total_days+=b
-    result=total_days%7
-    answer=day[result]
+    month_day=[31,29,31,30,31,30,31,31,30,31,30,31]
+    weekday=7
+    total_day=0
+    
+    if a>1:
+        for i in range(a-1):
+            total_day+=month_day[i]
+            
+    total_day+=b
+    result=total_day%weekday
+    return day[result]
+        
+        
+    
+    
+    
     return answer
