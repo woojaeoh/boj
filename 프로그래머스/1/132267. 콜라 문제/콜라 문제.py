9b311cc5e2a1a43dcb1ad42d3721944cbd358957
woojaeoh <166394273+woojaeoh@users.noreply.github.com>
2024-11-06 16:03:58 +0900

    [level 1] Title: 콜라 문제, Time: 0.38 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,9 +1,10 @@
 def solution(a, b, n):
-    answer= 0 #총 받게된 콜라병 수
+    answer=0
+    
     while(n>=a):
-        remain_bottle=n%a   #남은 콜라의 개수
-        n=(n//a)*b #마트에서 받은 콜라의 개수       
-        answer+=n #받은 콜라의 개수 업데이트
-        n+=remain_bottle #현재 가지고 있는 콜라의 개수 업데이트
+        remain_bottle=n%a
+        n=(n//a)*b
+        answer+=n
+        n+=remain_bottle
         
     return answer
