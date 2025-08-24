ac6efe6204665529bae7fb9c6fe89b28a1bbe06c
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2025-08-24 22:18:35 +0900

    [Silver III] Title: 프린터 큐, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,29 +1,28 @@
 import sys
 from collections import deque
 
-cases = int(sys.stdin.readline())
+input =sys.stdin.readline
+cases = int(input())
 
 for _ in range(cases):
-    n, m= map(int,sys.stdin.readline().split())
-    priority = deque(map(int,sys.stdin.readline().split()))
+    n, m= map(int,input().split())
+    priority = list(map(int,input().split()))
+    
+    q = deque(enumerate(priority))
+    
+    target =sorted(priority, reverse=True)
+    
     count = 0 #반복문을 제어하는 count
     
-    while priority:
-        max_num = max(priority) #가장 큰 수 확인
-        front = priority.popleft()
-        m -= 1
+    while True:
+        i, p = q[0]
         
-        if max_num  == front:
+        if p  == target[count]:
+            q.popleft()
             count +=1
-            if m < 0:
+            if i == m:
                 print(count)
                 break
         else:
-            priority.append(front)
-            if m < 0:
-                m = len(priority)-1
-
-#가장 큰 해결책: 구하려는 인덱스m에 대한 값을 pop한 후 맨 뒤에 append함으로써
-# max값을
-#해당 인덱스가 큐에서 몇번쨰의 중요도를 가지고 있는지 파악
-        
+            idx,pri = q.popleft()
+            q.append((idx,pri))
