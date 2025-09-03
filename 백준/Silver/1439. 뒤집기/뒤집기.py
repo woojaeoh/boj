bc4df0f9d3f686e6cf72067224b27e14c6044fc5
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2025-09-03 23:00:37 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,18 +1,15 @@
-s = input()
-count0 = 0  #연속적인 0을 셀 변수 (전부 1로 바꾸는 경우)
-count1 = 0  #연속적인 1을 셀 변수 (전부 0으로 바꾸는 경우)
+import sys
 
-if s[0] == '0':  # 첫 번째 정수에 대한 카운트 진행
-  count0 += 1
-else:  # 첫 번째 정수에 대한 카운트 진행
-  count1 += 1
+input = sys.stdin.readline
 
-for i in range(len(s) - 1):
-  if s[i] != s[i + 1]:
-    # 다음 수에서 1로 바뀌는 경우
-    if s[i + 1] == '1':
-      count1 += 1
-    else:  #다음수에서 0으로 바뀌는 경우
-      count0 += 1
+num = input()
 
-print(min(count0, count1))
+k = num[0]
+count = 0
+
+for i in num:
+    if i != k:
+        count += 1
+        k = i
+        
+print(count//2)
