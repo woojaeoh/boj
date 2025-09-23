a5129054d7a82ad57eb8c6ab3f45769e66eabcdf
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2025-09-24 01:34:04 +0900

    [level 1] Title: 실패율, Time: 18.77 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,21 +1,20 @@
 def solution(N, stages):
-    answer=[]
-    total_players=len(stages)
-    #조건 1. 분모 줄이기
-    for i in range(1,N+1):
-        if total_players !=0:
-            stage_count=stages.count(i)
-            fail=stage_count/total_players         
-            total_players-=stage_count
-        
+    
+    memo = {}
+    total = len(stages) #20만
+    result =[0]* 200001
+    for stage in stages:
+        result[stage] += 1 
+    
+    for i in range(1, N+1):
+        if total >0:
+            memo[i] = result[i] / total
         else:
-            fail=0
+            memo[i] = 0
+        total -= result[i]
+    
+    
+    answer = sorted(memo, key =lambda x : memo[x], reverse = True)
             
-        answer.append((i,fail))
-        
-    answer=sorted(answer,key=lambda t:t[1],reverse=True)
-    #조건 2. 실패율이 큰 순서대로 배열에 집어 넣기.
-    answer=[i[0] for i in answer]
-    #조건 3. 
     
     return answer
