86715c7723f44f229db5b83064bf9ac23e1bf924
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-01-23 14:00:56 +0900

    [Gold I] Title: 구슬 탈출, Time: 56 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
@@ -3,72 +3,68 @@ from collections import deque
 
 input = sys.stdin.readline
 
-N, M  = map(int, input().split())
+N, M = map(int, input().split())
 
-grid = [input().rstrip() for _ in range(N)]
-
+board = [input().rstrip() for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
-# R과 B의 좌표 저장
-rx = ry = bx = by = 0
+rr = rc = br = bc =0
 for i in range(N):
     for j in range(M):
-        if grid[i][j] == 'R':
-            rx, ry = i, j
-        elif grid[i][j] == 'B':
-            bx, by = i, j
+        if board[i][j] == 'R':
+            rr, rc = i, j
+        elif board[i][j] == 'B':
+            br, bc = i, j
+            
+q = deque()
+q.append((rr, rc, br, bc, 0))
 
-def move(x, y , dx, dy):
-    
+visited =set()
+visited.add((rr, rc,br,bc))
+
+def move(r, c, dx, dy):
     count = 0
-    while grid[x + dx][y + dy] != "#" and grid[x][y] != 'O':
-        x += dx
-        y += dy
+    while board[r+dx][c+dy] != '#' and board[r][c] != 'O': 
+        r += dx
+        c += dy
         count += 1
         
-    return x, y, count
-    
-
-q = deque()
-q.append((rx, ry, bx, by , 0))
-visited = set()
-visited.add((rx, ry, bx, by))
+    return r, c, count
 
 while q:
-    
-    cur_rx, cur_ry, cur_bx, cur_by, depth = q.popleft()
+    cur_rr, cur_rc, cur_br ,cur_bc, depth = q.popleft()
     
     if depth >= 10:
         continue
-       
+    
+    
     for i in range(4):
+        next_rr, next_rc, cnt_r =  move(cur_rr, cur_rc, dx[i] , dy[i])
+        next_br ,next_bc, cnt_b =  move(cur_br, cur_bc, dx[i] , dy[i])
         
-        next_rx, next_ry, cnt_r = move(cur_rx, cur_ry , dx[i], dy[i])
-        next_bx, next_by, cnt_b = move(cur_bx, cur_by , dx[i], dy[i])
         
-        if grid[next_bx][next_by] == 'O':
+        if board[next_br][next_bc] == 'O':
             continue
         
-        if grid[next_rx][next_ry] == 'O':
+        if board[next_rr][next_rc] == 'O':
             print(1)
             exit()
-            
         
-        if next_rx == next_bx and next_ry == next_by:
+        if next_rr == next_br and next_rc == next_bc:
             if cnt_r > cnt_b:
-                next_rx -= dx[i]
-                next_ry -= dy[i]
+                next_rr -= dx[i]
+                next_rc -= dy[i]
             else:
-                next_bx -= dx[i]
-                next_by -= dy[i]
+                next_br -= dx[i]
+                next_bc -= dy[i]
                 
-        if (next_rx, next_ry, next_bx, next_by) not in visited:
-            visited.add((next_rx, next_ry, next_bx, next_by))
-            q.append((next_rx, next_ry, next_bx, next_by, depth+1))
-            
+        
+        if (next_rr, next_rc, next_br, next_bc) not in visited:
+            visited.add((next_rr, next_rc, next_br, next_bc))
+            q.append((next_rr, next_rc, next_br, next_bc, depth + 1))
+        
+        
+    
 print(0)
-                
-                
