4cab7fff9a69e69aed2516f7c7a419f76bff4d64
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-01-27 18:49:24 +0900

    Time: 459 ms (7.81%), Space: 21.4 MB (16.8%) - LeetHub

---
+++ LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+        def dijkstra(graph, start):
+            costs = {}
+            pq =[]
+            heapq.heappush(pq, (0, start))
+
+            while pq:
+                cur_cost, cur_node = heapq.heappop(pq)
+                if cur_node not in costs:
+                    costs[cur_node] = cur_cost
+                for next_node, cost in graph[cur_node]:
+                    if next_node not in costs:
+                        next_cost = cur_cost + cost
+                        heapq.heappush(pq,(next_cost, next_node))
+            for i in range(n):
+                if (i+1) not in costs:
+                    return 0
+            return max(costs.values())
+
+
+        graph =  defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v,w)) #next_node, cost
+        #print(graph)
+        result = dijkstra(graph,k)
+        return -1 if result == 0 else result
