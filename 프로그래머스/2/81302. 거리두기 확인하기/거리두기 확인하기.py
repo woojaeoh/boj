62a5ab2710f2aebbab39c8558e5705afbc658b33
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-01-27 13:32:32 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.11 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,77 +1,53 @@
 from collections import deque
 
 def solution(places):
-    result = []     
+    
+    answer = []
     for place in places:
-        result.append((1 if check(place) else 0 ))
-        
-    return result
+        answer.append(1 if check(place) else 0)
+    
+    return answer
 
 
 def check(place):
-    for r in range(5):
-        for c in range(5):
-            if place[r][c] == "P":
-                if not bfs(r, c, place):
+    
+    for i in range(5):
+        for j in range(5):
+            if place[i][j] == "P":
+                if not bfs(place, i, j):
                     return False
     
     return True
 
-
-
-def isValid(x, y, place, visited):
-    return 0<= x <5 and 0<= y< 5 and place[x][y] != "X" and not visited[x][y]
-
-
-def bfs(start_r, start_c, place):
+def bfs(place,r, c):
     
-    visited =[[False ]*5 for _ in range(5)]
     q = deque()
-    q.append((start_r, start_c, 0))
-    visited[start_r][start_c] = True
+    q.append((r,c,0))
+    visited =[[False]*5 for _ in range(5) ]
+    visited[r][c] = True
     
     while q:
-        r, c, dist = q.popleft()
-    
-        #직선거리 방문처리
-        for dr, dc in [(-1,0), (1, 0), (0, -1), (0, 1)]:
-            nr, nc = r + dr, c + dc
-
-            if not isValid(nr, nc, place, visited):
-                continue
-                
-            if dist > 1:
-                continue
-            
-            if place[nr][nc] == "P":
-                return False
-            
-            visited[nr][nc] = True
-            q.append((nr, nc, dist+1))
-                
-            
-    
-#     #대각선 방문처리
-#     #좌상
-#     if isValid(r, c-1, place) and isValid(r-1, c, place):
-#         if isValid(r-1,c-1, place):
-#             visited[r-1][c-1] = True
-    
-#     #상우
-#     if isValid(r-1, c, place) and isValid(r, c+1, place):
-#         if isValid(r-1,c+1, place):
-#             visited[r-1][c+1] = True
-    
-#     #우하
-#     if isValid(r, c+1, place) and isValid(r+1, c, place):
-#         if isValid(r+1,c+1, place):
-#             visited[r+1][c+1] = True
-            
-#     #하좌
-#     if isValid(r+1, c, place) and isValid(r, c-1, place):
-#         if isValid(r+1,c-1, place):
-#             visited[r+1][c-1] = True
-    
-    
+        cur_r, cur_c , dist  = q.popleft()
+        
+        for dr, dc in [(-1, 0), (1,0) , (0,-1), (0,1)]:
+            next_r = cur_r + dr
+            next_c = cur_c + dc
+            next_dist = dist+1
+        
+            if next_dist > 2:
+                continue   
+                    
+        
+            if 0<= next_r < 5  and 0<=  next_c < 5 :
+                if not visited[next_r][next_c]:
+                    
+                    if place[next_r][next_c] != "X":                        
+                        if place[next_r][next_c] == "P":
+                            return False
+                        else:
+                            q.append((next_r, next_c, dist+1))
+                            visited[cur_r][cur_c] = True
+                                                      
+        
     return True
-            
+                    
