9f1381b657e574d9a77dc20b6685078c2936da0e
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-02-02 16:55:25 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.66 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -2,28 +2,31 @@ import heapq
 def solution(jobs):
     
     n = len(jobs)
+    pq =[]
     jobs = sorted([(requested_at,response_time, i )for i,(requested_at, response_time) in enumerate(jobs)])
-    completed = 0
-    current_time = 0
-    job_idx =0
-    turnaround_time = 0
     
-    heap = []
+    time = 0 #현재 진행 시각
+    idx = 0
+    completed = 0 #완료한 작업 수
+    turnaround_time = 0 # 모든 작업들의 turnaround time의 합 
     
-    while completed < n:
-        
-        while job_idx < n and jobs[job_idx][0] <= current_time: #현재시점 기준 작업할 수 있는 노드들 가져오기.
-            (requested_at, response_time, i ) = jobs[job_idx]
-            heapq.heappush(heap,(response_time, requested_at, i))
-            job_idx += 1 
+    while completed < n : # 모든작업이 종료할 떄까지 # 500
+         
+            while idx < n and jobs[idx][0] <= time:
+                requested_at,response_time, i = jobs[idx]
+                heapq.heappush(pq, (response_time, requested_at , i))
+                idx += 1
             
-        if heap:
-            response_time, requested_at, _ = heapq.heappop(heap)
-            current_time += response_time
-            turnaround_time += (current_time - requested_at) 
-            completed += 1
-        else:
-            current_time = jobs[job_idx][0] #힙이 비었다면 , 다음 작업 요청의 시작시간으로 점프
-            
-    
+            if pq: # 수행할 작업이 있다면
+                work_time, requested_at, i = heapq.heappop(pq) # 그 작업을 수행한다. 우선순위에 맞게
+                time += work_time #시간은 현재 시간에 소요되는 작업 시간을 더해줌.
+                turnaround_time += time - requested_at # 작업이 끝나는 시간  - 처음에 큐에 들어온 시간.
+                completed += 1
+                
+            else: #수행할 작업이 없다면 -> 시간만 + 1
+                time = jobs[idx][0]
+      
     return turnaround_time // n
+            
+            
+            
