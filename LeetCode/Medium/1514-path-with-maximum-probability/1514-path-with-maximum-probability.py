b99824e9b4dab5c19195dd334a52f5dcae4cf0ff
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-02-03 14:28:23 +0900

    Time: 88 ms (66.92%), Space: 31 MB (31.67%) - LeetHub

--- LeetCode/Medium/1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ LeetCode/Medium/1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,32 +1,34 @@
-import heapq
 from collections import defaultdict
-
+import heapq
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        
+
+        #그래프 완성  -> 0: (1, 0.5) 
         graph = defaultdict(list)
         for i, (u, v) in enumerate(edges):
-            graph[u].append((succProb[i], v))
-            graph[v].append((succProb[i], u))
-
-        costs={}
+            graph[u].append((v, succProb[i]))
+            graph[v].append((u, succProb[i]))
+        
+        costs = {}
         pq =[]
-        heapq.heappush(pq, (-1, start_node, end_node)) #시작노드        
+        heapq.heappush(pq, (-1, start_node))
+        costs[start_node] = 0
 
         while pq:
-            weight , cur_node, end = heapq.heappop(pq)
-            if cur_node not in costs:
-                costs[cur_node] = -1 * weight
+            cost, cur_node = heapq.heappop(pq)
 
-            if cur_node == end:
-                return -1 * weight
-            
-            for cur_weight, next_node in graph[cur_node]:
+            if cur_node not in costs:
+                costs[cur_node] = -1 * cost
+
+            if cur_node == end_node:
+                return -1 * cost
+
+            for next_node, weight in graph[cur_node]:
                 if next_node not in costs:
-                    next_weight = weight * cur_weight
-                    heapq.heappush(pq, (next_weight, next_node, end))
-                    
-        for i in range(end_node+1):
-            if i not in costs:
-                return 0
-        
-        return -1
+                    next_cost  = weight * cost
+                    heapq.heappush( pq, (next_cost, next_node))
+
+                
+        return 0
+                 
