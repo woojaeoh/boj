a63f22aa2f2b9019ea82063cd7defe80a5a407c3
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-02-05 22:07:46 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.24 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,35 +1,48 @@
 def solution(new_id):
+    n = len(new_id)
     
-    new_id=new_id.lower() #1단계: 소문자로 변환 
-
-    tmp=""
-
-    for char in new_id:
-        if char.isdigit() or  char.isalpha() or char in ['-','_','.']:
-            tmp+=char #2단계: 특수문자 제거
-
-    new_id=tmp
-
-    while ".." in new_id:
-        new_id=new_id.replace("..",".") #3단계: 연속된 점 제거
-  
-    if len(new_id)>0 and new_id[0]==".": #4단계 .이 맨앞이나 뒤일경우 제거.
-        new_id=new_id[1:]
-     
-    if len(new_id)>0 and new_id[-1]==".":
-        new_id=new_id[:-1]
-     
-
-    if new_id=="":#5단계 빈 문자열인 경우 a추가
-        new_id='a'
-
-    if len(new_id)>15: #6단계 길이가 15이상인경우 앞의 15자리만 표시
-        new_id=new_id[:15]
-        if new_id[-1]=='.':
-            new_id=new_id[:-1]
-
-    while len(new_id)<3: #7단계..
-        new_id+=new_id[-1]
+    #1단계
+    result = new_id.lower()
+    
+    temp =""
+    #2단계
+    for c in result:
+        if c.isalpha() or c.isdigit() or c == "-" or  c == "_" or c == ".":
+            temp += c
+    result = temp
+    
+    #3단계 -> 마침표가 두번 연속된것을 하나의 점으로.
+    prev = ""
+    ans = ""
+    for num in result:
+        if num == ".": # 현재 값이 . 이라면
+            if prev == ".": # 그리고 이전 값도 . 이라면?
+                continue
+            
+        prev = num
+        ans += num
+    
+    result = ans
+    
+    #4단계
+    if len(result) > 0 and result[0] == '.' :
+        result = result[1:]
+    if len(result) > 0 and result[-1] == '.' :
+        result = result[:-1]
+    
+    #5단계
+    if not result:
+        result = "a"
+    
+    #6단계
+    if len(result) >= 16:
+        result = result[:15]
+        if result[-1] == ".":
+            result = result[:14]
+            
+    #7단계
+    while len(result) <= 2:
+        result += result[-1]
+          
         
-
-    return new_id
+    return result
