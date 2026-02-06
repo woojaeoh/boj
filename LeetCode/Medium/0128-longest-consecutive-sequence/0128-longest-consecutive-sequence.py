1e92fcc11becf72564c18ee250e3273669b2a916
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-02-06 14:05:54 +0900

    Time: 67 ms (20.55%), Space: 42.4 MB (5.25%) - LeetHub

--- LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,20 +1,22 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-
-        maximum = 0
-        num_set = set(nums)
-
-        for num in num_set:
-            if (num-1) not in num_set:
-                count = 1
-                current_num = num
-                while (current_num +1) in num_set:
-                    current_num += 1
-                    if current_num in num_set:
-                        count += 1
-                    else:
-                        break
-                maximum = max(maximum, count)
+        dict ={}
+        for num in nums:
+            dict[num] = True
         
+        maximum = 0
+        for num in set(nums):
+            if num-1 not in dict: #연속된 값 중 맨 첫 값만 시작지점으로 판단.
+                count = 1
+                next_num = num + 1
+                while True: 
+                    if next_num in dict: #다음 값이 dict안에 있는지 검사.
+                        count += 1
+                        next_num += 1
+                    else:
+                        maximum = max(maximum , count) 
+                        break
+
+    
         return maximum
             
