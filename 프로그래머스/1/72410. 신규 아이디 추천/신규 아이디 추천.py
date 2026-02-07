bdfaceea8f98fd268fba46911f4fe440155fc299
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-02-07 15:48:41 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.32 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,48 +1,34 @@
 def solution(new_id):
-    n = len(new_id)
     
     #1단계
-    result = new_id.lower()
+    new_id  = new_id.lower()
     
-    temp =""
     #2단계
-    for c in result:
-        if c.isalpha() or c.isdigit() or c == "-" or  c == "_" or c == ".":
-            temp += c
-    result = temp
-    
-    #3단계 -> 마침표가 두번 연속된것을 하나의 점으로.
-    prev = ""
-    ans = ""
-    for num in result:
-        if num == ".": # 현재 값이 . 이라면
-            if prev == ".": # 그리고 이전 값도 . 이라면?
-                continue
-            
-        prev = num
-        ans += num
-    
-    result = ans
+    temp =""
+    for num in new_id:
+        if num.isalpha() or num.isdigit() or num in "._-":
+            temp += num 
+    #3단계
+    while ".." in temp:
+        temp = temp.replace("..", ".") #replace는 새로운 문자열을 반환 -> 기존 문자열은 바뀌지 않음
     
     #4단계
-    if len(result) > 0 and result[0] == '.' :
-        result = result[1:]
-    if len(result) > 0 and result[-1] == '.' :
-        result = result[:-1]
+    temp = temp.strip(".")
     
     #5단계
-    if not result:
-        result = "a"
-    
-    #6단계
-    if len(result) >= 16:
-        result = result[:15]
-        if result[-1] == ".":
-            result = result[:14]
-            
-    #7단계
-    while len(result) <= 2:
-        result += result[-1]
-          
+    if len(temp) == 0:
+        temp += "a"
         
-    return result
+    #6단계
+    
+    if len(temp) >= 16:
+        temp= temp[:15]
+        if temp[-1] == ".":
+            temp = temp.rstrip(".")
+    
+    while len(temp) <= 2:
+        temp += temp[-1]
+        
+    new_id = temp
+    print(temp)
+    return new_id
