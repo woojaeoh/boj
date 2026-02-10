f82e80cbbbca0762019e28b1f7e871f6ff8de270
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-02-10 15:16:06 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.29 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,46 +1,26 @@
 def solution(today, terms, privacies):
+    dict ={}
     result =[]
-    term_dict = {}
+    def change_days(year, month , day):
+        days = day
+        days += year * 336
+        days += month * 28
+        return days
     
     for term in terms:
-        provision, valid = term.split() # A 6
-        term_dict[provision] = int(valid)
+        name, month = term.split()
+        dict[name] = int(month) * 28
     
+    cur_year, cur_month, cur_day = map(int, today.split('.'))
+    cur_days = change_days(cur_year, cur_month, cur_day)
+    print(cur_days)
     for i, p in enumerate(privacies):
-        date , provision = p.split()
-        year, month, day = map(int, date.split("."))
-        month += term_dict[provision]
-        
-        count = 0
-        if month > 12:
-            count = month // 12
-            year += count
-            month %= 12
-            if month == 0:
-                month = 12
-                year -= 1 
-                       
-        if day == 1 :
-            day = 28
-            if month == 1:
-                month = 12
-                year-= 1
-            else:
-                month -= 1
-        else:
-            day -= 1    
-            
-        t_year , t_month , t_day = map(int,today.split("."))
-        
-        if 2000<= t_year <= 2022 and year < t_year:
+        date, name = p.split()
+        year, month , day = map(int, date.split('.'))
+        limit_day = change_days(year, month, day)
+        end_days = limit_day + dict[name]
+        print(end_days)
+        if cur_days >= end_days:
             result.append(i+1)
-            continue
-        if 2000<= t_year <= 2022 and year == t_year and month < t_month:
-            result.append(i+1)
-            continue
-        
-        if 2000<= t_year <= 2022 and year == t_year and month == t_month and day < t_day:
-            result.append(i+1)
-            continue
             
     return result
