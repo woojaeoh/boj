0d007dc396d07b154376c2a70f90ab15f1fbf820
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-02-28 16:22:56 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.15 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,26 +1,32 @@
 def solution(today, terms, privacies):
-    dict ={}
-    result =[]
-    def change_days(year, month , day):
-        days = day
-        days += year * 336
-        days += month * 28
-        return days
+    #today  = "2022.05.19"
+    # terms = ["A 6", "B 12", "C 3"]
+    # privacies =["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+    
+    dict = {}
+    
+    def change_day(year, month , day):
+        real = year*12*28
+        real += month*28
+        real += day
+        
+        return real
     
     for term in terms:
-        name, month = term.split()
-        dict[name] = int(month) * 28
+        rule, month = term.split() # A 6
+        dict[rule] = change_day(0,int(month),0)
     
-    cur_year, cur_month, cur_day = map(int, today.split('.'))
-    cur_days = change_days(cur_year, cur_month, cur_day)
-    print(cur_days)
+    t_year, t_month, t_day = map(int, today.split("."))
+    today_day =  change_day(t_year, t_month, t_day)    
+    
+    result = []
     for i, p in enumerate(privacies):
-        date, name = p.split()
-        year, month , day = map(int, date.split('.'))
-        limit_day = change_days(year, month, day)
-        end_days = limit_day + dict[name]
-        print(end_days)
-        if cur_days >= end_days:
+        date, rule = p.split() # 2021.0502 , A
+        year , month, day = map(int, date.split(".")) # 2021, 05, 02
+        
+        real_day = change_day(year, month, day)
+        
+        if real_day + dict[rule] <= today_day:
             result.append(i+1)
-            
+    
     return result
