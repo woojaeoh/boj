e045ebaa131c2e41140b7daab57439221d33ae3c
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-02-28 15:40:43 +0900

    Time: 529 ms (57.53%), Space: 19.8 MB (51.51%) - LeetHub

--- LeetCode/Medium/0322-coin-change/0322-coin-change.py
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.py
@@ -1,17 +1,25 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')]* (amount +1) 
-        dp[0] = 0
-        
-        for coin in coins:
-            if coin <= amount:
-                dp[coin] = 1
+        # dp = [float('inf')]* (amount +1) 
+        # dp[0] = 0
              
+        # for i in range(amount+1):
+        #     for coin in coins:
+        #         if i - coin >= 0:
+        #             dp[i] = min(dp[i-coin] + 1, dp[i])
+        # print(dp[:amount+1])
+
+        
+        # return dp[amount] if dp[amount] != float('inf') else -1  
+
+        dp_table  = [float('inf')] * (amount+1)
+        dp_table[0] = 0
+
         for i in range(amount+1):
             for coin in coins:
                 if i - coin >= 0:
-                    dp[i] = min(dp[i-coin] + 1, dp[i])
-        print(dp[:amount+1])
+                    dp_table[i] =  min(dp_table[i], dp_table[i-coin]+ 1 )
 
         
-        return dp[amount] if dp[amount] != float('inf') else -1  
+        return dp_table[amount] if dp_table[amount] != float('inf') else -1
+
