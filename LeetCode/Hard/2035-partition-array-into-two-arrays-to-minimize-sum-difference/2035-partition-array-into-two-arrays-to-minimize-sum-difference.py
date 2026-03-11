39b09b70586f4e99743417c7d88b00e91381fbf9
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-03-11 23:18:35 +0900

    Time: 1455 ms (85.64%), Space: 22.3 MB (50.13%) - LeetHub

--- LeetCode/Hard/2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
+++ LeetCode/Hard/2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.py
@@ -1,27 +1,41 @@
+from itertools import combinations
+from bisect import bisect_left
+
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
-        n = len(nums)
-        total = sum(nums) # -6
-        avg = total / 2  # mean값
-        min_diff = float('inf')
-        
-        def backtracking(start, curr):
-            nonlocal min_diff
+        n = len(nums) // 2 
+        total = sum(nums) 
+        goal = total / 2  # mean값
 
-            if len(curr) == n//2 :
-                min_diff = min(min_diff, abs(sum(curr) - avg))
-                return        
+        min_diff = 300000001
+
+        def get_partial_sums(arr):
+            result = [[] for _ in range(len(arr) + 1)]
+            for count in range(len(arr)+1):
+                for comb in combinations(arr, count):
+                    result[count].append(sum(comb))
+            return result
+  
+
+        left_sums = get_partial_sums(nums[:n])
+        right_sums = get_partial_sums(nums[n:])
+
+        #왼쪽에서 k개선택 , 오른쪽에서 n-k개 선택
+        for k in range(n+1):
+            left = sorted(left_sums[k])
+            right = sorted(right_sums[n-k])
             
-            for i in range(start, n ):
-                curr.append(nums[i])
-                backtracking(i+1 , curr)
-                curr.pop()
-        
+            i , j = 0 , len(right)-1
 
-        backtracking(0, [])
+            while i < len(left) and j >= 0:
+                s = left[i] + right[j]
+                min_diff = min(min_diff, abs(total - 2*s))
 
-        return int(2 * abs(min_diff))
+                if 2*s > total:
+                    j -= 1
+                else:
+                    i += 1
+
+        return min_diff
+
             
-                        
-
-        
