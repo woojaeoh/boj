cae463209530413af8d16364e1b001eba4b47437
ohjaiwoo <166394273+woojaeoh@users.noreply.github.com>
2026-03-12 23:32:25 +0900

    [Gold II] Title: 스타트 택시, Time: 136 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.py
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.py
@@ -1,119 +1,140 @@
 import sys
-from collections import deque
+from collections import defaultdict, deque
+
+#6 3 15
+# 0 0 1 0 0 0
+# 0 0 1 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 1 0
+# 0 0 0 1 0 0
+# 6 5
+# 2 2 5 6
+# 5 4 1 6
+# 4 2 3 5
 
 input = sys.stdin.readline
 
-N, M, fuel = map(int, input().split())
+N, M, fuel = map(int, input().split()) #격자, 승객 수 , 초기 연료
 
-board = [list(map(int, input().split())) for _ in range(N)]
+graph = [list(map(int, input().split())) for _ in range(N)]
 
-taxi_r, taxi_c = map(int, input().split())
+taxi_r , taxi_c = map(int, input().split()) #택시 출발 지점
 taxi_r -= 1
 taxi_c -= 1
 
-passenger={} #승객의 시작위치 : 목적지를 저장하는 용도.
+passenger = {}
 
-for _ in range(M):
-    sr ,sc, dr, dc = map(int, input().split())
-    passenger[(sr-1, sc-1)] = (dr-1, dc-1)
+dx =[-1, 1, 0, 0]
+dy =[0, 0, -1 ,1]
+
+#코드 설계
+# 현재 지점에서 가장 가까운 승객의 좌표를 찾아야 한다. -> find_shortest_passenger
+
+def search_shortest_node_bfs(r, c): 
     
-
-
-def find_passenger_bfs(r,c):
+    if (r, c) in passenger:
+        return r, c, 0
     
-    q = deque()
-    q.append((r,c,0)) # r, c, 좌표
-    visited = [[False] * N for _ in range(N)]
-    visited[r][c] =True
+    visited =[[False] * N for _ in range(N)]
     candidate = []
-    min_dist = float('inf')
+    dist = float('inf')
+        
+    q = deque()
+    q.append((r, c, 0))
+    visited[r][c] = True
     
     while q:
-        cur_r, cur_c, dist = q.popleft()
+        cur_r, cur_c, cur_cost = q.popleft()
         
-        if min_dist < dist:
+        if dist < cur_cost: #더 큰 가중치는 더이상 탐색 x
             continue
-        
+                
         if (cur_r, cur_c) in passenger:
-            min_dist = min(min_dist, dist)
-            candidate.append((min_dist,cur_r,cur_c))
+            candidate.append((cur_r, cur_c))
+            dist =  min(dist, cur_cost)
             continue
-        
-        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-            next_r = cur_r + dx
-            next_c = cur_c + dy        
-            if 0 <= next_r < N and 0<= next_c < N:
-                if not visited[next_r][next_c] and board[next_r][next_c] == 0:
-                    q.append((next_r, next_c, dist + 1))
+                
+        for i in range(4):
+            next_r = cur_r + dx[i]
+            next_c = cur_c + dy[i]
+            
+            if 0 <= next_r < N and 0 <= next_c < N:
+                if graph[next_r][next_c] == 0 and not visited[next_r][next_c]:
                     visited[next_r][next_c] = True
+                    q.append((next_r, next_c, cur_cost + 1))
     
-    if not candidate:
-        print(-1)
-        exit()
-        
+    if len(candidate) == 0:
+        return -1, -1, -1
+
+    #candidate.sort(key=lambda x : (x[0], x[1]))
     candidate.sort()
+    x, y = candidate[0]
+        
+                        
+    return x, y ,dist
+
+
+def from_passenger_to_destinaiton(r, c, dest_r, dest_c): #승객 -> 목적지
     
-    return candidate[0][1], candidate[0][2], candidate[0][0] # 갈 수 있는 가장 빠른 손님의 r, c, dist
-
-
-def go_destination_bfs(r, c):
+    visited = [[False]*N for _ in range(N)]
     
     q = deque()
-    q.append((r,c,0)) # r, c, 좌표
-    visited = [[False] * N for _ in range(N)]
-    visited[r][c] =True
-    dest_r , dest_c = passenger[(r,c)]
+    q.append((r, c, 0))
+    visited[r][c] = True
+    
     while q:
-        cur_r, cur_c, dist = q.popleft()
-        
-        if cur_r == dest_r and cur_c ==dest_c:
-            return cur_r, cur_c, dist
-        
-        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-            next_r = cur_r + dx
-            next_c = cur_c + dy        
-        
-            if 0 <= next_r < N and 0<= next_c < N:
-                if next_r == dest_r and next_c ==dest_c:
-                    return next_r, next_c, dist+1
+        cur_r, cur_c, cur_cost = q.popleft()
+    
+        if (cur_r, cur_c) == (dest_r, dest_c):
+            return cur_cost
                 
-                if not visited[next_r][next_c] and board[next_r][next_c] == 0:
-                    q.append((next_r, next_c, dist + 1))
+        for i in range(4):
+            next_r = cur_r + dx[i]
+            next_c = cur_c + dy[i]
+            
+            if 0<= next_r < N and 0 <= next_c < N:
+                if graph[next_r][next_c] == 0 and not visited[next_r][next_c]:
                     visited[next_r][next_c] = True
+                    q.append((next_r, next_c, cur_cost+1))              
     
-    return None, None, -1
-    
+    return -1
 
-#메인실행.
-for i in range(M):
-    start_r, start_c , dist_s = find_passenger_bfs(taxi_r, taxi_c)
-   # print(i, fuel)
-    (taxi_r, taxi_c) = (start_r, start_c)
- #   print(start_r, start_c)
+
+
+for num in range(M): #승객들의 출발지, 목적지를 저장.
+    passenger_r , passenger_c, dest_r , dest_c = map(int, input().split()) #승객들의 출발지, 목적지 좌표
+    passenger_r -= 1
+    passenger_c -= 1
+    dest_r -= 1
+    dest_c -= 1
     
-    if fuel - dist_s >= 0:
-        fuel-= dist_s
-    else:
+    dist = from_passenger_to_destinaiton(passenger_r, passenger_c, dest_r, dest_c)
+    passenger[(passenger_r, passenger_c)] = (dest_r, dest_c, dist)
+    #여기서 (passenger_r, passenger_c) -> dest_r, dest_c까지 얼마나 걸리는지 저장해놓기?
+    
+  
+for _ in range(M):
+    r, c, cost = search_shortest_node_bfs(taxi_r, taxi_c) #초기 택시 좌표로부터 
+    
+    if fuel -cost < 0 or (r,c,cost) == (-1,-1,-1):
         print(-1)
         exit()
+    else:
+        fuel -= cost
+        taxi_r, taxi_c, cost = passenger[(r,c)]
+        if cost == -1 or fuel < cost:
+            print(-1)
+            exit()
+            
+        fuel -= cost
+        fuel += (cost * 2)
         
-    dest_r, dest_c , dist_d = go_destination_bfs(taxi_r, taxi_c)
-    if dist_d == -1:
-        print(-1)
-        exit()
-    (taxi_r, taxi_c) = (dest_r, dest_c)
-  #  print(taxi_r, taxi_c)
-    
-    
-    if fuel >= dist_d:
-        fuel -= dist_d
-        fuel += dist_d *2
-    else:
-        print(-1)
-        exit()
-    
-    del passenger[(start_r, start_c)]
-    
-#    print(passenger)
+        del passenger[(r,c)]
+        
 
 print(fuel)
+        
+    
+
+
